ab6226fc7a6ab90acde2f362f71793b40f6a087d
Joni Collinge <jonathan-collinge@hotmail.com>
2016-10-27 10:47:30 +0100

    Initial commit

---
+++ KPMGSample/Controllers/ReportsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KPMGSample.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KPMGSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KPMGSample.MvcApplication" Language="C#" %>
